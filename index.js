ce86888f959b8e9741e82b24e4a4384f70eb1658
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-20 21:09:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+const superbowlWin = (item) => { 
+
+  const yearWinOrLoss = item.find(x => x.result === "W");
+
+  if(typeof yearWinOrLoss === 'object') {
+    return yearWinOrLoss.year;
+  } else {
+    return yearWinOrLoss;
+  }
+
+}
+
+
+
